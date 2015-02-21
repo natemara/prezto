634fba917094487d01d4032a9fb9572afab67f65
Nate Mara <natemara@gmail.com>
2015-02-21 13:13:32 -0500

    Create start.zsh

---
+++ start.zsh
@@ -0,0 +1,8 @@
+git clone --recursive git@github.com:natemara/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+	ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+chsh -s /bin/zsh
